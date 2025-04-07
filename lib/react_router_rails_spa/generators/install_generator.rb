b20ba9b36abe8a6c4b1ed7177194b2e2f9380c33
Naofumi Kagami <naofumi@mac.com>
2025-04-08 00:50:36 +0900

    Added messages in the install generator

--- lib/react_router_rails_spa/generators/install_generator.rb
+++ lib/react_router_rails_spa/generators/install_generator.rb
@@ -6,17 +6,19 @@ module ReactRouterRailsSpa
       source_root File.expand_path("templates", __dir__)
 
       def create_react_router_app
-        say "Setting up React Router v7 SBA mode in Rails..."
+        say "Downloading React Router v7 ..."
         inside Rails.root do
           run "npx create-react-router@latest frontend --yes --no-git-init"
         end
       end
 
       def setup_rails_routes
+        say "Setting up Rails routes ..."
         route 'get "react/*path", to: "react#show"'
       end
 
       def create_react_controller
+        say "Creating the React bootstrap endpoint controller ..."
         create_file "app/controllers/react_controller.rb", <<-RUBY
           class ReactController < ApplicationController
             def show
@@ -31,15 +33,18 @@ module ReactRouterRailsSpa
       end
 
       def copy_rake_task
+        say "Copying Rake tasks ..."
         template "react.rake", "lib/tasks/react.rake"
       end
 
       def copy_react_router_configs
+        say "Copying React Router configurations ..."
         template "react-router.config.ts", "frontend/react-router.config.ts"
         template "vite.config.ts", "frontend/vite.config.ts"
       end
 
       def copy_react_app_files
+        say "Copying React Router application pages and utilities ..."
         template "home.tsx", "frontend/app/routes/home.tsx"
         directory "welcome", "frontend/app/welcome"
         template "csrf.ts", "frontend/app/utilities/csrf.ts"
