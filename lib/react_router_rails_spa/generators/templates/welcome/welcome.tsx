b9416dacf73b68c941708316ee720ab28a67af2f
Naofumi Kagami <naofumi@mac.com>
2025-04-09 11:21:03 +0800

    Modify Welcome page

--- lib/react_router_rails_spa/generators/templates/welcome/welcome.tsx
+++ lib/react_router_rails_spa/generators/templates/welcome/welcome.tsx
@@ -5,7 +5,7 @@ import railsLogo from "./rails-logo.svg";
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+      <div className="flex-1 flex flex-col items-center min-h-0">
         <header className="flex flex-row items-baseline gap-9 max-w-[100vw]">
           <div className="w-[300px]">
             <img
@@ -28,16 +28,18 @@ export function Welcome() {
             />
           </div>
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
+        <div className="mt-12 text-2xl text-gray-600">with the</div>
+        <h1 className="mt-6 text-3xl font-bold">react_router_rails_spa gem</h1>
+        <div className="mt-12 max-w-[300px] w-full space-y-6 px-4">
           <nav className="space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+            <p className="text-2xl leading-6 text-gray-700 dark:text-gray-200 text-center">
               What&apos;s next?
             </p>
             <div className="text-center">
               {resources.map(({ href, text }) => (
                 <a
                   key={href}
-                  className="block mb-2 leading-normal text-blue-700 hover:underline dark:text-blue-500"
+                  className="block mb-4 leading-tight text-blue-700 hover:underline dark:text-blue-500"
                   href={href}
                   target="_blank"
                   rel="noreferrer"
@@ -55,11 +57,19 @@ export function Welcome() {
 
 const resources = [
   {
-    href: "https://reactrouter.com/docs",
-    text: "React Router Docs",
+    href: "https://github.com/naofumi/react_router_rails_spa",
+    text: "Read the README for the react_router_rails_spa gem",
   },
   {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
+    href: "https://github.com/naofumi/react_router_rails_spa/documentation/introduction.md",
+    text: "Read the Introduction for the react_router_rails_spa gem",
+  },
+  {
+    href: "https://reactrouter.com/docs",
+    text: "Read the React Router Docs",
+  },
+  {
+    href: "https://reactrouter.com/docs",
+    text: "Read the Ruby on Rails Docs",
   },
 ];
